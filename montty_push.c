a0005c2e06a8df700979479461c43898b3f35405
root <abayomirobertonawole2@gmail.com>
2023-12-13 11:30:51 +0000

    spike

--- montty_push.c
+++ montty_push.c
@@ -1,21 +1,21 @@
 #include "monty.h"
 /**
- * f_push - add node to the stack
+ * f_push - Functions add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+	int n, m = 0, flag = 0;
 
 	if (bus.arg)
 	{
 		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
+			m++;
+		for (; bus.arg[m] != '\0'; m++)
 		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+			if (bus.arg[m] > 57 || bus.arg[m] < 48)
 				flag = 1; }
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
