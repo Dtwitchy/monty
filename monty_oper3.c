c6b39cc63c4608e208a4be1c0462accf4aeda23b
root <abayomirobertonawole2@gmail.com>
2023-12-13 16:49:25 +0000

    bread

--- monty_oper3.c
+++ monty_oper3.c
@@ -1,6 +1,28 @@
 #include "monty.h"
 
 /**
+ * f_pall - The prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void f_pall(stack_t **head, unsigned int counter)
+{
+        stack_t *h;
+        (void)counter;
+
+        h = *head;
+        if (h == NULL)
+                return;
+        while (h)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+        }
+}
+
+
+/**
  * f_swap - adds the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
