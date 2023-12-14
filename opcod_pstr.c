60ea69de8f449374d461bc1e240c8767f964a9f7
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-12-14 23:29:20 +0100

    okay

--- opcod_pstr.c
+++ opcod_pstr.c
@@ -2,14 +2,14 @@
 /**
  * f_pstr - prints the string starting at the top of the stack,
  * followed by a new
- * @head: stack head
- * @counter: line_number
+ * @head: pointer to head of stack
+ * @line_num: line number of current operation
  * Return: no return
 */
-void f_pstr(stack_t **head, unsigned int counter)
+void f_pstr(stack_t **head, unsigned int line_num)
 {
         stack_t *h;
-        (void)counter;
+        (void)line_num;
 
         h = *head;
         while (h)
