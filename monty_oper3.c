7659be7a4a5a2229efbb9fdacaf2819504e590b2
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-12-15 01:56:21 +0100

    monty

--- monty_oper3.c
+++ monty_oper3.c
@@ -2,10 +2,10 @@
 /**
  * f_pall - prints the stack
  * @head: stack head
- * @number: no used
+ * @counter: no used
  * Return: no return
 */
-void f_pall(stack_t **head, unsigned int number)
+void f_pall(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	(void)counter;
