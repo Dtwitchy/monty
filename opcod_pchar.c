2b706ccac12e3ed1aa832021678986e54ad6c2b2
root <abayomirobertonawole2@gmail.com>
2023-12-15 09:04:26 +0000

    lopest

--- opcod_pchar.c
+++ opcod_pchar.c
@@ -5,7 +5,7 @@
  * @head: stack head
  * @counter: line_number
  * Return: no return
-*/
+ */
 void f_pchar(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
@@ -21,6 +21,7 @@ void f_pchar(stack_t **head, unsigned int counter)
 	}
 	if (h->n > 127 || h->n < 0)
 	{
+
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
 		fclose(bus.file);
 		free(bus.content);
